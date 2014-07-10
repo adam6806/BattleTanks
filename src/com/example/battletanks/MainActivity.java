8288681d321663eff03119f401905f589cd31c19
PirateOfDW <asmith0935@gmail.com>
2014-07-09 20:37:46 -0600

    Bluetooth update

--- src/com/example/battletanks/MainActivity.java
+++ src/com/example/battletanks/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.battletanks;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,12 +13,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
 	private SensorManager mSensorManager;
 	private Sensor mGrav;
 	int[] sensorValues = {0,0,0};
+    private Button On, Off, Visible, list;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    private ListView lv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +39,60 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mGrav = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-	}
+
+        On = (Button) findViewById(R.id.button1);
+        Off = (Button) findViewById(R.id.button2);
+        Visible = (Button) findViewById(R.id.button3);
+        list = (Button) findViewById(R.id.button4);
+
+        lv = (ListView) findViewById(R.id.listView1);
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void on(View view) {
+
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on"
+                    , Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void list(View view) {
+
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+        for (BluetoothDevice bt : pairedDevices)
+            list.add(bt.getName());
+
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",
+                Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new ArrayAdapter
+                (this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+
+    }
+
+    public void off(View view) {
+
+        BA.disable();
+        Toast.makeText(getApplicationContext(), "Turned off",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void visible(View view) {
+
+        Intent getVisible = new Intent(BluetoothAdapter.
+                ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
